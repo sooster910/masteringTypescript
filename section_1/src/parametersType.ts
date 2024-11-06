cbc9a29b694e021ff40cc7747295f5f48f098e40
Hyunsu <hs9880@gmail.com>
2024-11-06 13:03:56 +0900

    feat: arrow function 과 function declaration의 다른 parameter타입 추론 예제 추가

--- section_1/src/parametersType.ts
+++ section_1/src/parametersType.ts
@@ -15,5 +15,26 @@ function func(foo:string, obj:{bar:number, bas:boolean}){
 type FuncParameter = Parameters<typeof func>
 // [foo:string, obj:{bar:number, bas:boolean}] 튜플로 추론됨 =>  명시적으로  고정되어 있음
 
-type test = [Expect<Equal<FuncParameter, [string, { bar:number; bas:boolean; }]>>]
+type TestFuncParameter = [Expect<Equal<FuncParameter, [string, { bar:number; bas:boolean; }]>>]
 
+/**
+ * 2.
+ * @param locationId
+ * @param details
+ */
+const getDetailedWeather = (
+    locationId: string,
+    details?: {
+        tempUnit?: 'C' | 'F';
+        includeForecast?: boolean;
+    },
+) => {
+};
+
+
+type GetDetailedWeather= Parameters <typeof getDetailedWeather>
+
+type test= Expect<Equal<GetDetailedWeather,[locationId:string,details?: {
+    tempUnit?: 'C' | 'F';
+    includeForecast?: boolean;
+} ] >>
